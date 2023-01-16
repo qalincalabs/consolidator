283b3cdf7b2eb0f7f40b1ea45077443fcebb0fc9
Olivier Wouters <olivier5741@gmail.com>
2023-01-16 18:25:15 +0100

    add testing

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # consolidator
+
 Consolidate data for logCiCa based on plural ids
+
+## TODOs
+
+* isSameAsConsolidation not working with mongo decimal (because toJSON is used ...)
---
+++ index.test.js
@@ -0,0 +1,40 @@
+import { Consolidator } from "./index.js";
+
+test("Main sample", () => {
+  const currentOrder = {
+    ids: ["R3I432I4"],
+    number: "R3I432I4",
+    lines: [
+      {
+        name: "Carrots",
+        quantity: {
+          value: 2,
+        },
+      },
+    ],
+  };
+
+  const updatedOrder = {
+    ids: ["R3I432I4"],
+    number: "R3I432I4",
+    lines: [
+      {
+        name: "Carrots",
+        quantity: {
+          value: 3,
+        },
+      },
+    ],
+  };
+
+  const consolidatedOrder = new Consolidator(updatedOrder, currentOrder);
+  const consolidationProfile = {
+    action: "setUnlessSourceEmpty",
+  };
+
+  console.log(
+    JSON.stringify(consolidatedOrder.consolidate(consolidationProfile), null, 2)
+  );
+
+  console.log(consolidatedOrder.isSameAsConsolidation(consolidationProfile));
+});
